887acb4c0e146a62ede0b73ceb3a470d98a69314
ChristianBaciu <christian3110@outlook.it>
2024-01-09 19:09:01 +0100

    nome cognome biglietto

--- main.js
+++ main.js
@@ -1,31 +1,44 @@
-// Scrivere un programma che chieda all’utente:
-// - Il numero di chilometri da percorrere
-// - Età del passeggero
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del
-// biglietto di viaggio, secondo le seguenti regole:
-// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// - va applicato uno sconto del 20% per i minorenni
-// - va applicato uno sconto del 40% per gli over 65.
+const nomeGognomeHtml = document.getElementById( "nomeCognome" )
+const kmHtml = document.getElementById( "km" )
+const etaHtml = document.getElementById( "eta" )
+const bottoneGeneraHtml = document.getElementById( "bottoneGenera" )
+const numeroCarrozzaHtml = document.getElementById( "numeroCarrozza" )
+const numeroCpHtml = document.getElementById( "numeroCp" )
+const bigliettoNascostoHtml = document.getElementById( "bigliettoNascosto" )
+const nomeBigliettoHtml = document.getElementById( 'nomeBiglietto' )
+const costoBigliettoHtml = document.getElementById( 'costoBiglietto' )
 
-// MILESTONE 1:
-// Iniziamo implementando il programma senza alcuna estetica: usando 
-// esclusivamente due input e un bottone (non stilizzati), realizziamo
-// le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa
-// da scrivere in console.
 
-// MILESTONE 2:
-// Solo una volta che il milestone 1 sarà completo e funzionante allora 
-// realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
-// Il recap dei dati e l'output del prezzo finale, andranno quindi 
-// stampati in pagina (il prezzo dovrà essere formattato con massimo 
-// due decimali, per indicare i centesimi sul prezzo). Questo richiederà
-// un minimo di ricerca.
+// per mettere in display-none la section biglietto
+bigliettoNascostoHtml.style.display = "none"
 
-let buttonSend = document.getElementById("genera")
 
-buttonSend.addEventListener ("click", function(){
-    let valueCampo = document.getElementById ("Nome").value
-    console.log (valueCampo)
+// al click del bottone attiva
+bottoneGeneraHtml.addEventListener( 'click', function() {
 
-    document.getElementById ("nomepasseggero").innerHTML = valueCampo
+    // per mettere in display-block la section biglietto
+    bigliettoNascostoHtml.style.display = "block"
+
+    // mette un numero casuale 
+    let numeroCarrozza = RandomNumber ( 0,10 )
+    let numeroCp = RandomNumber ( 100,10000 )
+    numeroCarrozzaHtml.innerHTML = numeroCarrozza
+    numeroCpHtml.innerHTML = numeroCp
+
+    // per mettere il nome cognome nel biglietto
+    nomeBigliettoHtml.innerHTML = nomeGognomeHtml.value
+    if( etaHtml.value === "minorenne" ){
+        costoBigliettoHtml.innerHTML = (kmHtml.value * 0.21) * 0.8
+  
+    }else if( etaHtml.value === "maggiorenne" ){  
+        costoBigliettoHtml.innerHTML = (kmHtml.value * 0.21) * 0.2
+  
+    }else if( etaHtml.value === "over65" ){  
+        costoBigliettoHtml.innerHTML = (kmHtml.value * 0.21) * 0.6
+    }
 })
+
+// genera un numero casuale 
+function RandomNumber( min,max ){
+    return Math.floor( Math.random()*(max-min + 1) + min)
+}
